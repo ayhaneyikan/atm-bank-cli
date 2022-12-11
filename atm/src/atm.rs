2e0d68b08a55d413afacc5b51e512e143ca7b647
Ayhan Eyikan <aeyikan@umd.edu>
2022-12-11 16:57:28 -0500

    restructured atm to be more "rust-like"

---
+++ atm/src/atm.rs
@@ -0,0 +1,186 @@
+use std::io::{self, Write};
+use regex::{Regex, Captures};
+use lazy_static::lazy_static;
+
+/* define atm constants */
+const MAX_USERNAME_SIZE: usize = 250usize;
+
+/*
+ * defines the ATM struct
+ */
+pub struct ATM {
+    prompt: String,
+    active_user: Option<String>,
+
+    /* networking state */
+
+    /* cryptographic state */
+    //secret_key
+}
+
+
+impl ATM {
+    /* initializer */
+    #[must_use]                                 // not exactly sure what this does
+    pub fn new() -> ATM {
+        ATM {
+            prompt: String::from("ATM: "),
+            active_user: None,
+        }
+    }
+
+
+    /* small helper functions */
+    pub fn get_prompt(&self) -> &String {
+        &self.prompt
+    }
+
+    /* updates active user and prompt */
+    fn login_user(&mut self, username: &str) {
+        self.active_user = Some(String::from(username));
+        self.prompt = String::from("ATM (") + username + "): ";
+        println!("Authorized\n");
+    }
+    fn logout_user(&mut self) {
+        println!("{} was logged out\n", self.active_user.as_ref().expect(
+            "Active user should never be none when logout_user() is called"
+        ));
+        self.active_user = None;
+        self.prompt = String::from("ATM: ");
+    }
+
+    fn is_logged_in(&self) -> bool {
+        if let None = self.active_user { false } else { true }
+    }
+
+
+
+    /* attempt to begin a user session */
+    pub fn process_begin_session(&mut self, user_input: &String) {
+        /* verify that no user is already logged in */
+        if self.is_logged_in() {
+            println!("A user is already logged in\n");
+            return
+        }
+
+        /* create static regular expression (creates it at most once) */
+        lazy_static! {
+            static ref RE: Regex = Regex::new("^begin-session ([a-zA-Z]+)$").expect(
+                "Error while compiling begin-session regular expression"
+            );
+        }
+    
+        /* ensure input matches */
+        if ! RE.is_match(user_input) {
+            println!("Usage: begin-session <user-name>\n");
+            return;
+        }
+        /* valid match */
+    
+        /* extract username from matched string */
+        let caps: Captures = RE.captures(user_input).unwrap();
+        let username: &str = caps.get(1).unwrap().as_str();
+        /* check if username is within max size */
+        if username.len() > MAX_USERNAME_SIZE {
+            println!("Error: username must be {} characters or less\n", MAX_USERNAME_SIZE);
+            return;
+        }
+        
+        /* check if they exist communiate with bank */    /* TEMP */
+        if username != "ayhan" && username != "addison" {
+            println!("No such user\n");
+            return;            
+        }
+    
+        /* prompt for PIN */
+        print!("  PIN: ");
+        io::stdout().flush().unwrap(); // flush prompt
+        /* capture PIN */
+        let mut pin: String = String::new();
+        io::stdin().read_line(&mut pin).expect("Failed to read PIN from stdin");
+        pin.pop();  // remove newline
+        /* check if provided PIN matches */
+        let failed_authorization: String = String::from("Not authorized\n");
+        lazy_static! {
+            static ref PIN_RE: Regex = Regex::new("^[0-9]{4}$").unwrap();
+        }
+        if ! PIN_RE.is_match(&pin) {
+            println!("{}", failed_authorization);
+            return;
+        }
+    
+        /* validate PIN with bank */                     /* TEMP */
+        if pin != "1188" {
+            println!("{}", failed_authorization);
+            return;
+        }
+    
+        /* update login state */
+        self.login_user(username);
+    }
+
+
+    pub fn process_withdraw(&mut self, user_input: &String) {
+        /* verify there is a user logged in */
+        if ! self.is_logged_in() {
+            println!("No user logged in\n");
+            return;
+        }
+
+        /* create static regular expression (creates it at most once) */
+        lazy_static! {
+            static ref RE: Regex = Regex::new("^withdraw ([0-9]+)$").expect(
+                "Error while compiling withdraw regular expression"
+            );
+        }
+    
+        /* ensure input matches */
+        if ! RE.is_match(user_input) {
+            println!("Usage: withdraw <amt>\n");
+            return;
+        }
+    
+        /* extract withdrawal amount from input */
+        let caps: Captures = RE.captures(user_input).unwrap();
+        let amount_string: &str = caps.get(1).unwrap().as_str();
+        
+        /* check if valid u64 */
+        let amount: u64;
+        if let Ok(extracted_value) = amount_string.parse::<u64>() {
+            amount = extracted_value;
+        } else {
+            println!("Error: your requested withdrawal amount is too large for our wee little bank to handle\n");
+            return;
+        }
+        
+        /* attempt withdrawal from bank */              /* TEMP */
+        if amount > 1_000_000u64 {
+            println!("Insufficient funds\n");
+            return;
+        }
+        println!("${} dispensed\n", amount);
+    }
+    
+    
+    pub fn process_balance(&mut self) {
+        /* verify there is a user logged in */
+        if ! self.is_logged_in() {
+            println!("No user logged in\n");
+            return;
+        }
+
+        /* no regular expression needed */
+    
+        /* retreive balance from the bank */
+    }
+
+    pub fn process_end_session(&mut self) {
+        /* check if there's a user logged in */
+        if ! self.is_logged_in() {
+            println!("No user logged in\n");
+            return;
+        }
+
+        self.logout_user();
+    }
+}
--- atm/src/main.rs
+++ atm/src/main.rs
@@ -1,44 +1,72 @@
+use std::{env, io::{self, Write}, net::{TcpStream, TcpListener}};
+
+use rand::{rngs::OsRng, RngCore};
+use x25519_dalek::{StaticSecret, PublicKey};
+
+mod atm;
+use crate::atm::ATM;
+
+// for encryption sources see: 
+//  https://cryptography.rs/
+//  https://kerkour.com/end-to-end-encryption-key-exchange-cryptography-rust
+// based on these recommendations, I decided to utilize:
+//  x25519-dalek        v1.2.0   for key exchange: https://crates.io/crates/x25519-dalek
+//  blake2              v0.10.5  for KDF: https://crates.io/crates/blake2
+//  XChaCha20-Poly1305  v0.10.1  for encryption: https://docs.rs/chacha20poly1305/latest/chacha20poly1305/
+
+/* define program constants */
+const ROUTER_ADDRESS: &str = "127.0.0.1:32000";
+const ATM_ADDRESS:    &str = "127.0.0.1:32001";
+
+const XCHACHA20_POLY1305_KEY_SIZE: usize = 32usize;     // 32 byte key
+const XCHACHA20_POLY1305_NONCE_SIZE: usize = 24usize;   // 24 byte nonce
+
+
 /*
  * ATM entry point
  */
-
-use std::{env, io::{self, Write}, default};
-use lazy_static::lazy_static;
-use regex::{Regex, Captures};
-use crypto;
-
-
-/*
- * defines the ATM struct
- */
-
-struct ATM {
-    prompt: String,
-    active_user: Option<String>,
-
-    /* networking state */
-
-    /* cryptographic state */
-}
-
-
 fn main() {
     // retreive command line argument iterator and convert to vec
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        panic!("The ATM expects exactly one argument: a \"<>.atm\" file. Found {} arguments instead", args.len());
+        println!("\nThe ATM expects exactly one argument: a \"<>.atm\" file. Found {} arguments instead", args.len());
+        return;
     }
+
+
+    /* generate key pair */
+
+    // create and initialize nonce array
+    // let mut nonce = [0u8; XCHACHA20_POLY1305_NONCE_SIZE];
+    // OsRng.fill_bytes(&mut nonce);  // randomize nonce values
+
+    // let private_key: StaticSecret = StaticSecret::new(OsRng);
+    // let public_key: PublicKey = PublicKey::from(&private_key);
+
+    // /* open connection to the router */
+    // let mut stream: TcpStream = TcpStream::connect(ROUTER_ADDRESS).expect(
+    //     "Error trying to connect to the router"
+    // );
+
+    // let listener: TcpListener = TcpListener::bind(ATM_ADDRESS).expect(
+    //     "Error creating atm listener"
+    // );
+    
+    // while let Err(_) = stream.write(public_key.as_bytes()) {}
+
+    // listener.accept().expect("oops");
+    
+
+    // return;
+    
     
     /* initialize atm struct */
-    let mut atm: ATM = ATM {
-        prompt: String::from("ATM: "),
-        active_user: None,
-    };
+    let mut atm: ATM = ATM::new();
 
     /* read in user input */
     let mut user_input: String = String::new();
-    
-    print!("{}", atm.prompt);  // initial prompt
+
+    print!("{}", atm.get_prompt());  // initial prompt
     io::stdout().flush().unwrap();  // flush output buffer to terminal
     while io::stdin().read_line(&mut user_input).expect("Failed to read line from stdin") > 0 {
         /* remove newline from user input */
@@ -51,38 +79,16 @@ fn main() {
 
         /* check for valid command and call appropriate helper function */
         if user_input.starts_with("begin-session") {
-            /* check if there's a user logged in */
-            match atm.active_user {
-                Some(_) => println!("A user is already logged in\n"),
-                None => process_begin_session(&mut atm, &user_input),
-            }
+            atm.process_begin_session(&user_input);                             /* DO THIS FOR OTHERS */
         }
         else if user_input.starts_with("withdraw") {
-            /* check if there's a user logged in */
-            match atm.active_user {
-                Some(_) => process_withdraw(&mut atm, &user_input),
-                None => println!("No user logged in\n"),
-            }
+            atm.process_withdraw(&user_input);
         }
         else if user_input == "balance" {
-            /* check if there's a user logged in */
-            match atm.active_user {
-                Some(_) => process_balance(&mut atm, &user_input),
-                None => println!("No user logged in\n"),
-            }
+            atm.process_balance();
         }
         else if user_input == "end-session" {
-            /* check if there's a user logged in */
-            match atm.active_user {
-                None => println!("No user logged in\n"),
-                Some(username) => {
-                    /* log this user out */
-                    println!("{} logged out\n", username);
-                    /* reset prompt and user data */
-                    atm.prompt = String::from("ATM: ");
-                    atm.active_user = None;
-                },
-            }
+            atm.process_end_session();
         }
         else {
             println!("Invalid command\n");
@@ -90,114 +96,9 @@ fn main() {
 
 
         /* reprompt user */
-        print!("{}", atm.prompt);
+        print!("{}", atm.get_prompt());
         io::stdout().flush().unwrap(); // flush prompt
         /* clear user input buffer before next read */
         user_input.clear();
     }
-}
-
-
-fn process_begin_session(atm: &mut ATM, user_input: &String) {
-    /* create static regular expression (creates it at most once) */
-    lazy_static! {
-        static ref RE: Regex = Regex::new("^begin-session ([a-zA-Z]+)$").expect(
-            "Error while compiling begin-session regular expression"
-        );
-    }
-    /* define maximum allowed username size */
-    const MAX_USERNAME_SIZE: usize = 250usize;
-
-    /* ensure input matches */
-    if ! RE.is_match(user_input) {
-        println!("Usage: begin-session <user-name>\n");
-        return;
-    }
-    /* valid match */
-
-    /* extract username from matched string */
-    let caps: Captures = RE.captures(user_input).unwrap();
-    let username: &str = caps.get(1).unwrap().as_str();
-    /* check if username is within max size */
-    if username.len() > MAX_USERNAME_SIZE {
-        println!("Error: username must be {} characters or less\n", MAX_USERNAME_SIZE);
-        return;
-    }
-    
-    /* check if they exist communiate with bank */    /* TEMP */
-    if username != "ayhan" && username != "addison" {
-        println!("No such user\n");
-        return;            
-    }
-
-    /* prompt for PIN */
-    print!("  PIN: ");
-    io::stdout().flush().unwrap(); // flush prompt
-    /* capture PIN */
-    let mut pin: String = String::new();
-    io::stdin().read_line(&mut pin).expect("Failed to read PIN from stdin");
-    pin.pop();  // remove newline
-    /* check if provided PIN matches */
-    let failed_authorization: String = String::from("Not authorized\n");
-    lazy_static! {
-        static ref PIN_RE: Regex = Regex::new("^[0-9]{4}$").unwrap();
-    }
-    if ! PIN_RE.is_match(&pin) {
-        println!("{}", failed_authorization);
-        return;
-    }
-
-    /* validate PIN with bank */                     /* TEMP */
-    if pin != "1188" {
-        println!("{}", failed_authorization);
-        return;
-    }
-
-    /* update login state */
-    atm.active_user = Some(String::from(username));
-    atm.prompt = String::from("ATM (") + &username + &String::from("): ");
-    println!("Authorized\n");
-}
-
-
-fn process_withdraw(atm: &mut ATM, user_input: &String) {
-    /* create static regular expression (creates it at most once) */
-    lazy_static! {
-        static ref RE: Regex = Regex::new("^withdraw ([0-9]+)$").expect(
-            "Error while compiling withdraw regular expression"
-        );
-    }
-
-    /* ensure input matches */
-    if ! RE.is_match(user_input) {
-        println!("Usage: withdraw <amt>\n");
-        return;
-    }
-
-    /* extract withdrawal amount from input */
-    let caps: Captures = RE.captures(user_input).unwrap();
-    let amount_string: &str = caps.get(1).unwrap().as_str();
-    
-    /* check if valid u64 */
-    let amount: u64;
-    if let Ok(extracted_value) = amount_string.parse::<u64>() {
-        amount = extracted_value;
-    } else {
-        println!("Error: your requested withdrawal amount is too large for our wee little bank to handle\n");
-        return;
-    }
-    
-    /* attempt withdrawal from bank */              /* TEMP */
-    if amount > 1_000_000u64 {
-        println!("Insufficient funds\n");
-        return;
-    }
-    println!("${} dispensed\n", amount);
-}
-
-
-fn process_balance(atm: &mut ATM, user_input: &String) {
-    /* no regular expression needed */
-
-    /* retreive balance from the bank */
 }
